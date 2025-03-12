f42157e63337c8f7cf10c589429de017f1cf09dc
N-Echelibe <162185242+N-Echelibe@users.noreply.github.com>
2025-03-12 09:51:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,45 @@
 import express from 'express';
-import pkg from 'pg';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import { createClient } from "@supabase/supabase-js";
+import bodyParser from 'body-parser';
+
 
 dotenv.config();
 
-const { Pool } = pkg;
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-
-const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT
-});
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/api/hello', (req, res) => {
   res.json({message: 'Hello from the backend!'});
+});
+
+app.get('/api/data', async (req, res) => {
+  const {data, error} = await supabase.from('userInfo').select('*');
+  if (error) {
+    res.json({error: error.message});
+  } else {
+    res.json(data);
+  }
+})
+
+app.post('/api/login', async (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const {user, error} = await supabase.from('userInfo').select('*').and(`email.eq.${email}, password.eq.${password}`);
+  if (error) {
+    res.json({error: error.message})
+  } else {
+    res.json(user);
+  }
 })
 
 app.listen(port, () => {
